73ba592925ad52e1595caf6d73f929facc6b20c9
unclebusy <deniskalagin@gmail.com>
2024-04-19 20:42:52 +0600

    add animation to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect } from 'react';
+
 function Footer() {
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-      <footer className="page-footer green darken-3">
-        <div className="footer-copyright">
-          <div className="container">
-            © 2014 All rights reserved
-            <a className="grey-text text-lighten-4 right" href="https://github.com/unclebusy/website-shop">This project on my Github</a>
+        <footer className={`page-footer green darken-3 ${isHidden ? 'hide' : ''}`}>
+          <div className="footer-copyright">
+            <div className="container">
+              © 2014 All rights reserved
+              <a className="grey-text text-lighten-4 right" href="https://github.com/unclebusy/website-shop">This project on my Github</a>
+            </div>
           </div>
-        </div>
-      </footer>
-  )
+        </footer>
+  );
 }
 
 export default Footer;
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
 function Header() {
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="page-header green darken-3">
-      <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">Website Shop</a>
-        <ul className="right hide-on-med-and-down">
-          <li><a href="https://github.com/unclebusy/website-shop">This project on my Github</a></li>
-        </ul>
-      </div>
-    </nav>
-  )
+      <nav className={`page-header green darken-3 ${isHidden ? 'hide' : ''}`}>
+        <div className="nav-wrapper">
+          <a href="#!" className="brand-logo">Website Shop</a>
+          <ul className="right hide-on-med-and-down">
+            <li><a href="https://github.com/unclebusy/website-shop">This project on my Github</a></li>
+          </ul>
+        </div>
+      </nav>
+  );
 }
 
-export default Header;
+export default Header
